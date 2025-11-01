729f8b87f811e6b03d0ad5e96cabc9cc54edf229
EvrkMs <evrk.msn@gmail.com>
2025-11-01 11:38:46 +0300

    add auto add certificate

--- Program.cs
+++ Program.cs
@@ -1,6 +1,48 @@
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.UseKestrel(o =>
+{
+    o.ListenAnyIP(5001, listen =>
+    {
+        listen.UseHttps(https =>
+        {
+            https.ServerCertificate = EphemeralCert.Create();
+        });
+    });
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
+
+static class EphemeralCert
+{
+    public static X509Certificate2 Create()
+    {
+        using var rsa = System.Security.Cryptography.RSA.Create(2048);
+        var req = new CertificateRequest(
+            "CN=auth-host",
+            rsa,
+            System.Security.Cryptography.HashAlgorithmName.SHA256,
+            System.Security.Cryptography.RSASignaturePadding.Pkcs1);
+
+        var san = new SubjectAlternativeNameBuilder();
+        san.AddDnsName("safe-host");
+        san.AddDnsName("localhost");
+        san.AddIpAddress(IPAddress.Loopback);
+        req.CertificateExtensions.Add(san.Build());
+        req.CertificateExtensions.Add(new X509BasicConstraintsExtension(false, false, 0, false));
+        req.CertificateExtensions.Add(new X509KeyUsageExtension(
+            X509KeyUsageFlags.DigitalSignature | X509KeyUsageFlags.KeyEncipherment, false));
+
+        var now = DateTimeOffset.UtcNow.AddMinutes(-5);
+        var cert = req.CreateSelfSigned(now, now.AddYears(5));
+        // Rewrap to ensure Kestrel can access the private key across platforms
+        return new X509Certificate2(cert.Export(X509ContentType.Pfx));
+    }
+}
